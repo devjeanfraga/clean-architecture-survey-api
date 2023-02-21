38f1a2665675a4458780bbdbd964d02621533915
Jean Fraga <jeandevjs@gmail.com>
2023-02-21 13:19:53 -0300

    feat: ensure SignUpController return 400 if no name is provider

---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,15 @@
+import { SignUpController } from "./signup";
+
+describe( "SignUp Controller", () => {
+  it("Should return 400 if name is not provider", () => {
+    const sut = new SignUpController(); 
+    const httpRequest = {
+      email: 'any@email',
+      password: 'any-password',
+      confirmPassword: 'any-password'
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);  
+  }); 
+})
