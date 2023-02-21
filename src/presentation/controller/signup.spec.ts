7a1cea872652cc8921c8a7a1f215d33a1af42824
Jean Fraga <jeandevjs@gmail.com>
2023-02-21 14:01:11 -0300

    test: ensure SignUpController return an error if no email is provider

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,8 +1,9 @@
 import { SignUpController } from "./signup";
 
 describe( "SignUp Controller", () => {
+  const sut = new SignUpController();
+
   it("Should return 400 if name is not provider", () => {
-    const sut = new SignUpController(); 
     const httpRequest = {
       email: 'any@email',
       password: 'any-password',
@@ -13,4 +14,15 @@ describe( "SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400); 
     expect(httpResponse.body).toEqual(new Error("Missing param: name"));
   }); 
+
+  it("Should return 400 if email is not provider", () => {
+    const httpRequest = {
+      name: 'any-name',
+      password: 'any-password',
+      confirmPassword: 'any-password'
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: email"))
+  })
 })
