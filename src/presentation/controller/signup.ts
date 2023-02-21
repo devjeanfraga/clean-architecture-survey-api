261ec0fe8c3cec691b55d6a4d11bea4ab6de3a46
Jean Fraga <jeandevjs@gmail.com>
2023-02-21 14:00:48 -0300

    feat: ensure SignUpController return an error if no email is provider

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,8 +1,14 @@
 export class SignUpController {
   handle(httpResquest: any): any {
+    if (!httpResquest.name)
     return {
       statusCode: 400,
       body: new Error("Missing param: name")
+    };
+    if (!httpResquest.email)
+    return {
+      statusCode: 400,
+      body: new Error("Missing param: email")
     };
   }
 }
