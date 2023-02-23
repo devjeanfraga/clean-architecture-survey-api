de86a53c330b0a90a004e95b17cba128bd2b432c
Jean Fraga <jeandevjs@gmail.com>
2023-02-23 14:45:57 -0300

    feat: ensure SignupController returns a badRequest if missing some params

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,16 +1,14 @@
+import { MissingParamError } from '../errors/missing-param-error';
+import { badRequest } from '../http-helpers/http-helpers';
 import {Controller} from '../protocols/protocol-controller';
+import { HttpRequest, HttpResponse } from '../protocols/protocol-http';
 
 export class SignUpController implements Controller {
-  handle(httpResquest: any): any {
-    if (!httpResquest.name)
-    return {
-      statusCode: 400,
-      body: new Error("Missing param: name")
-    };
-    if (!httpResquest.email)
-    return {
-      statusCode: 400,
-      body: new Error("Missing param: email")
-    };
-  }
-}
+  handle(httpResquest: HttpRequest): Promise<HttpResponse> {
+   
+    for (const field of ["name", "email", "password", "confirmPassword"]) {
+      if( !httpResquest.body[field] ) return new Promise((resolve, reject) => resolve(badRequest(new MissingParamError(field))))
+    }
+    return null; 
+  }    
+} 
