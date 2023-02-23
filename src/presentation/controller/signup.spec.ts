1d5661f4104e9d7ea6520ed2871ae1dbaffe9a2b
Jean Fraga <jeandevjs@gmail.com>
2023-02-23 16:42:39 -0300

    test: ensure SignUpController calls add method by addAccount with correct values

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,11 +1,69 @@
 import { SignUpController } from "./signup";
 import {HttpRequest, HttpResponse} from '../protocols/protocol-http';
-import {badRequest} from '../http-helpers/http-helpers';
+import {badRequest,serverError, ok} from '../http-helpers/http-helpers';
 import {MissingParamError} from '../errors/missing-param-error';
 
-describe( "SignUp Controller", () => {
-  const sut = new SignUpController();
 
+interface AccountModel {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+} 
+
+interface AddAccountModel {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+} 
+
+interface AddAccount {
+  add (values: AddAccountModel):Promise<AccountModel>
+}
+
+const httpRequest: HttpRequest = {
+  body: {
+    name: 'any-name',
+    email: 'any@mail',
+    password: 'any-password',
+    confirmPassword: 'any-password',
+  }
+};
+
+const makeAddAccount = (): AddAccount => {
+  class AddAccountStub implements AddAccount {
+     async add (values: AddAccountModel): Promise<AccountModel> {
+      return new Promise( (resolve, rejects) => resolve({
+          id: "any-id",
+          name: "any-name",
+          email: "any-email",
+          password: "any-password"
+        })
+      );
+     }
+  }
+
+  return new AddAccountStub(); 
+}; 
+
+interface SutTypes {
+  sut: SignUpController; 
+  addAccountStub: AddAccount
+}
+
+const makeSut = (): SutTypes => {
+  const addAccountStub = makeAddAccount()
+  const sut = new SignUpController(addAccountStub);
+  return {
+    sut,
+    addAccountStub 
+  };
+}
+  
+
+
+describe( "SignUp Controller", () => {
 
   it("Should return 400 if name is not provider", async () => {
     const httpRequest: HttpRequest = {
@@ -15,7 +73,7 @@ describe( "SignUp Controller", () => {
         confirmPassword: 'any-password'
       }
     };
-
+    const { sut } = makeSut();
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('name')));
   }); 
@@ -28,6 +86,8 @@ describe( "SignUp Controller", () => {
         confirmPassword: 'any-password'
       }
     };
+
+    const { sut } = makeSut();
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   }); 
@@ -40,6 +100,8 @@ describe( "SignUp Controller", () => {
         confirmPassword: 'any-password'
       }
     };
+
+    const { sut } = makeSut();
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password'))); 
   }); 
@@ -52,7 +114,23 @@ describe( "SignUp Controller", () => {
         password: 'any-password'
       }
     };
+
+    const { sut } = makeSut();
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('confirmPassword'))); 
-  })
+  }); 
+
+  it( "Shoud calls add method by AddAccount with correct values", async () => {
+    const {sut, addAccountStub} = makeSut();
+    const addSpy = jest.spyOn(addAccountStub, 'add'); 
+
+    await sut.handle(httpRequest); 
+     expect(addSpy).toHaveBeenCalledWith({
+      name: 'any-name',
+      email: 'any@mail',
+      password: 'any-password',
+      confirmPassword: 'any-password',
+    }); 
+  });
+
 }); 
