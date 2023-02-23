b2add5ea32cffd839fa3b29cc388403087d39294
Jean Fraga <jeandevjs@gmail.com>
2023-02-23 16:42:00 -0300

    feat: ensure SignUpController calls add method by addAccount with correct values

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -3,12 +3,38 @@ import { badRequest } from '../http-helpers/http-helpers';
 import {Controller} from '../protocols/protocol-controller';
 import { HttpRequest, HttpResponse } from '../protocols/protocol-http';
 
+interface AccountModel {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+} 
+
+interface AddAccountModel {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+} 
+
+interface AddAccount {
+  add (values: AddAccountModel): Promise<AccountModel>
+}
+
+
 export class SignUpController implements Controller {
-  handle(httpResquest: HttpRequest): Promise<HttpResponse> {
+  
+  constructor (private readonly addAccount: AddAccount) {
+
+  }
+
+  async handle(httpResquest: HttpRequest): Promise<HttpResponse> {
    
     for (const field of ["name", "email", "password", "confirmPassword"]) {
       if( !httpResquest.body[field] ) return new Promise((resolve, reject) => resolve(badRequest(new MissingParamError(field))))
-    }
+    } 
+    const {name, email, password, confirmPassword} = httpResquest.body; 
+    await this.addAccount.add({ name, email, password, confirmPassword }); 
     return null; 
   }    
 } 
