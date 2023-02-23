2b09bed4b2b8146795f110cf42cb70e8b69af290
Jean Fraga <jeandevjs@gmail.com>
2023-02-23 14:46:45 -0300

    test: ensure SignupController returns a badRequest if missing some params

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,28 +1,58 @@
 import { SignUpController } from "./signup";
+import {HttpRequest, HttpResponse} from '../protocols/protocol-http';
+import {badRequest} from '../http-helpers/http-helpers';
+import {MissingParamError} from '../errors/missing-param-error';
 
 describe( "SignUp Controller", () => {
   const sut = new SignUpController();
 
-  it("Should return 400 if name is not provider", () => {
-    const httpRequest = {
-      email: 'any@email',
-      password: 'any-password',
-      confirmPassword: 'any-password'
+
+  it("Should return 400 if name is not provider", async () => {
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any@email',
+        password: 'any-password',
+        confirmPassword: 'any-password'
+      }
     };
 
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400); 
-    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('name')));
   }); 
 
-  it("Should return 400 if email is not provider", () => {
-    const httpRequest = {
-      name: 'any-name',
-      password: 'any-password',
-      confirmPassword: 'any-password'
+  it("Should return 400 if email is not provider", async () => {
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any-name',
+        password: 'any-password',
+        confirmPassword: 'any-password'
+      }
     };
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: email"))
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  }); 
+
+  it("Should return 400 if password is not provider", async () => {
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any-name',
+        email: 'any@mail',
+        confirmPassword: 'any-password'
+      }
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password'))); 
+  }); 
+
+  it("Should return 400 if password is not provider", async () => {
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any-name',
+        email: 'any@mail',
+        password: 'any-password'
+      }
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('confirmPassword'))); 
   })
-})
+}); 
