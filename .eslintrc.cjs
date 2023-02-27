4b5b5d51fd8922503d6532f43f2fb47eb1305416
Jean Fraga <jeandevjs@gmail.com>
2023-02-27 15:56:38 -0300

    chore: add new config on eslint and lintstaged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,4 +3,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  rules: {
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": "off",
+  }
 };
