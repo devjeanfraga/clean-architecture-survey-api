95242478fa1e206d888a2ca2518807cd5742d445
Jean Fraga <jeandevjs@gmail.com>
2023-02-27 22:02:51 -0300

    test: ensure BcryptAdapter calls hash method with correct password and salt

---
+++ src/infra/bcrypt-adapter.spec.ts
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt';
+import { Hasher } from '../data/protocols/hasher';
+import { BcryptAdapter } from './bcrypt-adapter';
+
+jest.mock('bcrypt', () => ({
+  hash(): Promise<string> {
+    return new Promise(resolve => resolve("hashed-password"));
+  }
+}));
+
+const makeHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    hash(value: string): Promise<string> { return Promise.resolve("hashed-password"); }
+  }
+  return new HasherStub();
+};
+
+interface SutTypes {
+  sut: BcryptAdapter;
+  hasherStub: Hasher;
+}
+
+const makeSut = (): SutTypes => {
+  const salt = 12; 
+  const hasherStub = makeHasher();
+  const sut = new BcryptAdapter(salt);
+  return {
+    sut,
+    hasherStub
+  };
+
+}
+describe( "BcryptAdapter", () => {
+  it("Should calls hash method with correct values", () => {
+     const {sut} = makeSut();
+     const salt = 12;
+     const hashSpy = jest.spyOn(bcrypt, "hash"); 
+     sut.hash('any-password'); 
+     expect(hashSpy).toHaveBeenCalledWith("any-password", salt); 
+  });
+ });
