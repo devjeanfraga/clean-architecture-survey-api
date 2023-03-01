4959c5c1e5996458751dd4b91a6860ee2d39a500
Jean Fraga <jeandevjs@gmail.com>
2023-03-01 15:03:27 -0300

    feat: ensure content type middleware return default content-type json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { jsonParser } from "../middlewares/json-parser";
 import { cors } from "../middlewares/cors";
+import { contentType } from "../middlewares/content-type";
 
 export default (app: Express) => {
   app.use(jsonParser);
   app.use(cors);
+  app.use(contentType);
 }; 
