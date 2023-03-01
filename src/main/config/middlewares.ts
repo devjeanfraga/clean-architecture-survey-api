b1c8e049aa62f8204a1d03508d5f0a69463d78e2
Jean Fraga <jeandevjs@gmail.com>
2023-03-01 15:00:54 -0300

    feat: ensure Cors middleware middleware enable cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import { jsonParser } from "../middlewares/json-parser";
+import { cors } from "../middlewares/cors";
 
 export default (app: Express) => {
-  app.use(jsonParser); 
+  app.use(jsonParser);
+  app.use(cors);
 }; 
