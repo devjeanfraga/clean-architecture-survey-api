c29250a6228f26069339386844ec7ed07f879b9e
Jean Fraga <jeandevjs@gmail.com>
2023-03-06 14:51:30 -0300

    test: ensure LogDecorator call handle method controller with correct values

---
+++ src/main/decorator/decorator.spec.ts
@@ -0,0 +1,45 @@
+import { serverError } from "../../presentation/http-helpers/http-helpers";
+import { Controller } from "../../presentation/protocols/protocol-controller"
+import { HttpRequest, HttpResponse } from "../../presentation/protocols/protocol-http"
+import { LogDecorator } from "./log-decorator";
+
+const { fakeRequest, fakeResponse } = global;
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      return new Promise(resolve => resolve(fakeResponse)); 
+    }
+  }
+  return new ControllerStub();
+};
+
+interface SutTypes {
+  sut: LogDecorator;
+  controllerStub: Controller;
+}
+
+const  makeSut = (): SutTypes => {
+  const controllerStub = makeController();
+  const sut = new LogDecorator(controllerStub);
+  return {
+    sut,
+    controllerStub
+  };
+};  
+
+describe("LogDecorator", () => {
+  it("Should call handle method of controller", async () => {
+    const {sut, controllerStub} = makeSut();
+    const handleSpy = jest.spyOn(controllerStub, 'handle');
+    
+    await sut.handle(fakeRequest);
+    expect(handleSpy).toHaveBeenCalledWith(fakeRequest);
+  });
+
+  it("Should return the same response of controller", async () => {
+    const {sut} = makeSut();
+    const res = await sut.handle(fakeRequest);
+    expect(res).toEqual(fakeResponse)
+  })
+})
