40f0a1f3aeee09ea4778727753cdd7a4db419398
Jean Fraga <jeandevjs@gmail.com>
2023-03-07 19:19:12 -0300

    feat: add login validation factory

---
+++ src/main/factories/login-validation-factory.ts
@@ -0,0 +1,19 @@
+import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { Validation } from "../../validations/protocols/validation";
+import { ValidationCompareFields } from "../../validations/validation-compare-fields";
+import { ValidationEmail } from "../../validations/validation-email";
+import { ValidationRequireFields } from "../../validations/validation-require-fields.";
+import { ValidationsComposite } from "../../validations/validations-composite";
+
+export const makeLoginValidation = (): ValidationsComposite => {
+  const validations: Validation[] = [];
+  
+  const fields = ['name', 'email', 'password', 'confirmPassword']
+  for ( const field of fields ) validations.push(new ValidationRequireFields(field));
+  
+  const emailValidator =  new EmailValidatorAdapter();
+  validations.push(new ValidationEmail('email', emailValidator));
+  validations.push(new ValidationCompareFields('password', 'confirmPassword'));
+
+  return new ValidationsComposite(validations); 
+};
