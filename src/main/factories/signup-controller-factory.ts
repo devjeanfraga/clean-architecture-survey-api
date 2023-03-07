dd74c757068dae4e7090ba60ab95bc53491fc7c9
Jean Fraga <jeandevjs@gmail.com>
2023-03-07 19:20:13 -0300

    feat: add ValidationComposite

--- src/main/factories/signup-controller-factory.ts
+++ src/main/factories/signup-controller-factory.ts
@@ -6,16 +6,15 @@ import { SignUpController } from "../../presentation/controller/signup";
 import { Controller } from "../../presentation/protocols/protocol-controller";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
 import { LogErrorDecorator } from "../decorator/log-error-decorator";
+import { makeLoginValidation } from "./login-validation-factory";
 
-export const makeSignUpController = (): Controller => {
-  const emailValidatorAdapter = new EmailValidatorAdapter();
-  
+export const makeSignUpController = (): Controller => {  
   const salt = 12;
   const bcryptAdapter = new BcryptAdapter(salt);
   const accountRepository = new AccountRepository();
 
   const dbAddAccount = new DbAddAccount(bcryptAdapter,accountRepository);
-  const signupController = new SignUpController(emailValidatorAdapter,dbAddAccount);
+  const signupController = new SignUpController( makeLoginValidation(), dbAddAccount);
 
   const logMongoRespository = new LogMongoRepository();
   return  new LogErrorDecorator(signupController, logMongoRespository); 
--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,34 +1,25 @@
-import { InvalidParamError } from '../errors/invalid-param-error';
-import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest, ok, serverError } from '../http-helpers/http-helpers';
 import {Controller} from '../protocols/protocol-controller';
 import { HttpRequest, HttpResponse } from '../protocols/protocol-http';
 import { AddAccount } from '../../domain/usecases/add-account';
-import {EmailValidator} from '../protocols/email-validator'
+import { Validation } from '../../validations/protocols/validation';
 
 
 export class SignUpController implements Controller {
   
   constructor (
-    private readonly emailValidator: EmailValidator,
+    private readonly validations: Validation,
     private readonly addAccount: AddAccount
-    ) {}
+  ) {}
 
   async handle(httpResquest: HttpRequest): Promise<HttpResponse> {
-   
     try {
-      for (const field of ["name", "email", "password", "confirmPassword"]) {
-        if( !httpResquest.body[field] ) return badRequest(new MissingParamError(field));
-      } 
-      
-      if (httpResquest.body["password"] !== httpResquest.body["confirmPassword"]) return badRequest(new InvalidParamError("confirmPassword"))
-      
+      const error = this.validations.validate(httpResquest.body);
+      if(error) return badRequest(error);
+
       const {name, email, password} = httpResquest.body; 
+      const account = await this.addAccount.add({ name, email, password});
 
-      const isValidEmail = await this.emailValidator.isValid(email);
-      if (!isValidEmail) return badRequest(new InvalidParamError('email'));
-
-      const account = await this.addAccount.add({ name, email, password}); 
       return ok(account);
     } catch (err) {
       return serverError(err);
