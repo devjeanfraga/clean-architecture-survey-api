c2728267d7e2be2137da770279b2a3f50c38579b
Jean Fraga <jeandevjs@gmail.com>
2023-03-07 16:58:46 -0300

    test: ensure ValidationEmail call isValid EmailValidator method with correct email

---
+++ src/validations/validation-email.spec.ts
@@ -0,0 +1,32 @@
+import { InvalidParamError } from "../presentation/errors/invalid-param-error";
+import { EmailValidator } from "../presentation/protocols/email-validator";
+import { ValidationEmail } from "./validation-email";
+
+const makeEmailvalidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true; 
+    }
+  }
+  return new EmailValidatorStub(); 
+};
+
+interface SutTypes { 
+  sut: ValidationEmail;
+  emailValidatorStub: EmailValidator  
+}
+const makeSut = (): SutTypes => {
+  const emailValidatorStub =  makeEmailvalidator();
+  const sut = new ValidationEmail("email", emailValidatorStub);
+  return { sut, emailValidatorStub };
+};
+
+describe("ValidationEmail", () => {
+  it("Should calls isValid method with correct email", async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const spyIsValid = jest.spyOn( emailValidatorStub, 'isValid' );
+    sut.validate({email: 'any@mail.com'}); 
+    expect(spyIsValid).toHaveBeenCalledWith('any@mail.com'); 
+  });
+
+});
