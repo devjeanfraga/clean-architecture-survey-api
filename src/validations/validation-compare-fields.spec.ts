bd31ba27f469c74adeeaed803403a92ecd70c3d6
Jean Fraga <jeandevjs@gmail.com>
2023-03-07 15:45:13 -0300

    test: ensure ValidationCompareFields returns null on success

--- src/validations/validation-compare-fields.spec.ts
+++ src/validations/validation-compare-fields.spec.ts
@@ -13,5 +13,11 @@ describe("ValidationCompareFields", () => {
     const error = sut.validate({field: "any-field", fieldToCompare: 'any-compare-field'}); 
     expect(error).toEqual(new InvalidParamError("field"));
   });
+
+  it("Should return null if Validation success", async () => {
+    const { sut } = makeSut();
+    const res = sut.validate({field: "any-field", fieldToCompare: 'any-field'}); 
+    expect(res).toBeFalsy(); 
+  })
 });
 
