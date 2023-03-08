a290b0bef4e5d05dad210d34208f07f71bb15e33
Jean Fraga <jeandevjs@gmail.com>
2023-03-08 15:30:12 -0300

    refactor: remove unused code

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,4 @@
 import bcrypt from 'bcrypt';
-import { Hasher } from '../../data/protocols/hasher';
-import { HasherCompare } from '../../data/protocols/hasher-compare';
 import { BcryptAdapter } from './bcrypt-adapter';
 
 jest.mock('bcrypt', () => ({
@@ -13,39 +11,14 @@ jest.mock('bcrypt', () => ({
   }
 }));
 
-const makeHasher = (): Hasher => {
-  class HasherStub implements Hasher, HasherCompare {
-
-    hash(value: string): Promise<string> { 
-      return Promise.resolve("hashed-password"); 
-    }
-
-    compare(value: string, encrypted: string): Promise<boolean> {
-      return Promise.resolve(true);
-    }
-  }
-
-  return new HasherStub();
-};
-
-interface SutTypes {
-  sut: BcryptAdapter;
-  hasherStub: Hasher;
-}
-
-const makeSut = (): SutTypes => {
+const makeSut = (): BcryptAdapter => {
   const salt = 12; 
-  const hasherStub = makeHasher();
   const sut = new BcryptAdapter(salt);
-  return {
-    sut,
-    hasherStub
-  };
-
-}
+  return sut;
+};
 describe( "BcryptAdapter", () => {
   it("Should calls hash method with correct values", () => {
-     const {sut} = makeSut();
+     const sut = makeSut();
      const salt = 12;
      const spyHash = jest.spyOn(bcrypt, "hash"); 
      sut.hash('any-password'); 
@@ -53,20 +26,20 @@ describe( "BcryptAdapter", () => {
   });
 
   it ("Should return a hashed password if hash on success", async () => {
-    const {sut} = makeSut();
+    const sut = makeSut();
     const response = await sut.hash("any-password");
     expect(response).toBe("hashed-password"); 
   });
 
   it("Should throws if hash method throws", async () => {
-    const {sut} = makeSut();
+    const sut = makeSut();
     jest.spyOn(bcrypt, "hash").mockImplementationOnce(() => { throw new Error(); }); 
     const response = sut.hash('any-password'); 
     await expect(response).rejects.toThrow(); 
   });
 
   it("Should calls compare method with correct values", () => {
-    const {sut} = makeSut();
+    const sut = makeSut();
     const spyCompare = jest.spyOn(bcrypt, 'compare'); 
     sut.compare('any-password', 'hashed-password'); 
     expect(spyCompare).toHaveBeenCalledWith("any-password", "hashed-password"); 
