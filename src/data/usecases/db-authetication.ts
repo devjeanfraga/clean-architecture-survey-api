a7a13247a03f11df70bd529b1fc1dd342866eec8
Jean Fraga <jeandevjs@gmail.com>
2023-03-08 11:06:26 -0300

    feat: ensure DbAuthentication call compare HasherCompare method with correct values

--- src/data/usecases/db-authetication.ts
+++ src/data/usecases/db-authetication.ts
@@ -1,13 +1,19 @@
 import { LoadAccountByEmailRepository } from "../protocols/load-account-by-email-repository";
 import {Authentication, AuthenticationModel} from "../../domain/usecases/authentication" 
+import { HasherCompare } from "../protocols/hasher-compare";
 
 export class DbAuthentication implements Authentication {
   constructor (
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hasherCompare: HasherCompare
   ) {}
 
   async auth (credentials: AuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(credentials.email);
-    if ( !account ) return null; 
+    const isValidPassword = account && await this.hasherCompare.compare(credentials.password, account.password); 
+    if (!isValidPassword) {
+      return null 
+    }
+    return null   
   }
 }
