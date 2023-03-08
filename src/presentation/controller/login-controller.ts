98a5f008ffec6b14c08d0d71b0bf0870dfb60063
Jean Fraga <jeandevjs@gmail.com>
2023-03-07 22:37:47 -0300

    feat: ensure LoginController return an Error if Validation return some Error

--- src/presentation/controller/login-controller.ts
+++ src/presentation/controller/login-controller.ts
@@ -1,11 +1,14 @@
 import { Validation } from "../../validations/protocols/validation";
+import { badRequest } from "../http-helpers/http-helpers";
 import { Controller } from "../protocols/protocol-controller";
 import { HttpRequest, HttpResponse } from "../protocols/protocol-http";
 
 export class LoginController implements Controller {
   constructor (private readonly validation: Validation) {}
+  
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body);
+    if ( error )  return badRequest(error);
     return null; 
   }
 }
