242b6245229a336c8ba0a5e8625e9a18e2ac5d26
Jean Fraga <jeandevjs@gmail.com>
2023-03-10 11:49:44 -0300

    test: ensure POST /login returns 401 ANAUTHORIZED if email not exists

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -7,10 +7,16 @@ describe('POST /login', () => {
   beforeAll(async () => {
     await MongoHelper.connect(global.__MONGO_URI__);
   });
+
+  beforeEach(async () => {
+    collection = MongoHelper.getCollection('accounts');
+    await collection.deleteMany({});
+  })
+
   afterAll(async () => {
     await MongoHelper.disconnect();
   });
-  it('Should return accessToken', async () => {
+  it('Should return status code 200 if success', async () => {
     const password = await bcrypt.hash('123', 12); 
     const fakeAccount = {
       name: 'any-name',
@@ -28,5 +34,15 @@ describe('POST /login', () => {
         password: '123'
       });
     expect(response.statusCode).toBe(200)
+  });
+
+  it('Should return status code 401 if email not exists', async () => {
+    const response = await global.testRequest
+      .post('/login')
+      .send({
+        email: 'any@mail.com',
+        password: '123'
+      });
+    expect(response.statusCode).toBe(401)
   });
 })
