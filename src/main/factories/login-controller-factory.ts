e2da7f8a21ace11ea0474fbd95a40d934acb9b91
Jean Fraga <jeandevjs@gmail.com>
2023-03-09 21:02:38 -0300

    feat: create login controller factory

---
+++ src/main/factories/login-controller-factory.ts
@@ -0,0 +1,21 @@
+import { DbAuthentication } from "../../data/usecases/db-authetication";
+import { BcryptAdapter } from "../../infra/cryptography/bcrypt-adapter";
+import { JwtAdapter } from "../../infra/cryptography/jwt-adapter";
+import { AccountRepository } from "../../infra/db/mongodb/account/account-repository";
+import { LogMongoRepository } from "../../infra/db/mongodb/logs/log-mongo-repository";
+import { LoginController } from "../../presentation/controller/login-controller";
+import { Controller } from "../../presentation/protocols/protocol-controller";
+import { LogErrorDecorator } from "../decorator/log-error-decorator";
+import { makeLoginValidation } from "./login-validation-factory";
+
+export const makeLoginController = (): Controller => {
+  const secretKey = 'Some-Key';
+  const jwtAdapter = new JwtAdapter(secretKey);
+  const salt = 12; 
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountRepository = new AccountRepository();
+  const dbAuthentication = new DbAuthentication(accountRepository, bcryptAdapter, jwtAdapter, accountRepository);
+  const logErrorRepository = new LogMongoRepository();
+  const loginController = new LoginController(makeLoginValidation(), dbAuthentication);
+  return new LogErrorDecorator(loginController, logErrorRepository);
+}; 
