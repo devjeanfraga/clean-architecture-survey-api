a3fb75611a25c6cdd78c1e04c092549769fac5c7
Jean Fraga <jeandevjs@gmail.com>
2023-03-10 12:54:31 -0300

    refactor: move signup factory to a single folder

--- src/main/routes/sign-up-routes.ts
+++ src/main/routes/sign-up-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { makeRouteAdapter } from '../apapters/express-adapter';
-import { makeSignUpController } from '../factories/signup-controller-factory';
+import { makeSignUpController } from '../factories/signup/signup-controller-factory';
 
 export default (router: Router): void => {
   router.post('/signup', makeRouteAdapter(makeSignUpController()));
