2fb21a693cf4451f73af5c488737382a3b385db1
Jean Fraga <jeandevjs@gmail.com>
2023-03-10 12:55:40 -0300

    refactor: move login factory to a single folder

---
+++ src/main/factories/login/login-validation-factory.ts
@@ -0,0 +1,15 @@
+import { EmailValidatorAdapter } from "../../../utils/email-validator-adapter";
+import { Validation } from "../../../validations/protocols/validation";
+import { ValidationEmail } from "../../../validations/validation-email";
+import { ValidationRequireFields } from "../../../validations/validation-require-fields.";
+import { ValidationsComposite } from "../../../validations/validations-composite";
+
+export const makeLoginValidation  = (): Validation => {
+  const validations: Validation[] = [];
+  const emailValidator = new EmailValidatorAdapter();
+
+  for(const field of ['email', 'password']) validations.push(new ValidationRequireFields(field));
+  validations.push(new ValidationEmail('email', emailValidator));
+
+  return new ValidationsComposite(validations);
+}; 
