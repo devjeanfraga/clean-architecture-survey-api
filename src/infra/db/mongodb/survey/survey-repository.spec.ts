6234a436902064191399296d3d814314e57a7135
Jean Fraga <jeandevjs@gmail.com>
2023-03-12 15:27:54 -0300

    test: ensure SurveyRepository add a survey

---
+++ src/infra/db/mongodb/survey/survey-repository.spec.ts
@@ -0,0 +1,36 @@
+import { Collection, ObjectId } from "mongodb";
+import { MongoHelper } from "../mongo-helpers";
+import { SurveyRepository } from "./survey-repository";
+
+const dataSurvey = {
+  question: 'any-question',
+  answers: [
+    { answer: 'any-answer-01', image: 'http://localhost:8080/any-image'},
+    { answer: 'any-answer-02', image: 'http://localhost:8080/any-image'},
+    { answer: 'any-answer-03'}
+  ],
+  date: 'any-date'
+};
+
+const sut = new SurveyRepository();
+describe('SurveyRepository', () => {
+  let surveyCollection: Collection;
+  beforeAll( async () => {
+  await MongoHelper.connect(global.__MONGO_URI__);   
+  });
+
+  beforeEach(async () => {
+    surveyCollection =  MongoHelper.getCollection('surveys');
+    surveyCollection.deleteMany({});
+  })
+
+  afterAll(async () =>{
+    await MongoHelper.disconnect();
+  });
+
+  it('Should add a survey on success', async () => {
+    sut.addSurvey(dataSurvey);
+    const promise = await surveyCollection.countDocuments(); 
+    expect(promise).toBe(1); 
+  })
+})
