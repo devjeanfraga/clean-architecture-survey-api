c303ac6a0683871bd6a447655b2cd62dc213dfd9
Jean Fraga <jeandevjs@gmail.com>
2023-03-13 21:52:25 -0300

    feat: ensure /add-survey route returns 403 if not exists x-access-token

--- src/main/routes/add-survey-routes.ts
+++ src/main/routes/add-survey-routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { makeRouteAdapter } from "../apapters/express-adapter";
+import { makeMiddlewareAdapter } from "../apapters/middleware-adapter";
 import { makeAddSurveyController } from "../factories/survey/add-survey/add-survey-controller-factory";
+import { makeAuthMiddleware } from "../factories/survey/add-survey/auth-middleware-factory";
 export default (router: Router): void => {
-  router.post('/add-survey', makeRouteAdapter(makeAddSurveyController())); 
+  const authMiddleware = makeMiddlewareAdapter(makeAuthMiddleware())
+  router.post('/add-survey', authMiddleware, makeRouteAdapter(makeAddSurveyController())); 
 }
