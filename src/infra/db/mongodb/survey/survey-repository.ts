d071e576f55cf71415f9accc19a8a5ce71943426
Jean Fraga <jeandevjs@gmail.com>
2023-03-16 12:29:11 -0300

    feat: ensure SurveyRepository returns a list of surveys on success

--- src/infra/db/mongodb/survey/survey-repository.ts
+++ src/infra/db/mongodb/survey/survey-repository.ts
@@ -1,11 +1,22 @@
 import { MongoHelper } from "../mongo-helpers";
 import { AddSurveyModel, AddSurveyRepository } from "../../../../data/usecases/db-add-survey-protocols";
+import { LoadSurveysRepository, SurveyModel } from "../../../../data/usecases/db-load-surveys-protocols";
 
-export class SurveyRepository implements AddSurveyRepository {
+export class SurveyRepository implements AddSurveyRepository, LoadSurveysRepository {
   constructor(){}
 
   async addSurvey(dataSurvey: AddSurveyModel): Promise<void> {
     const collection =  MongoHelper.getCollection('surveys');
     await collection.insertOne(dataSurvey); 
+  }
+
+  async loadSurveys(): Promise<SurveyModel[]> {
+    const res: SurveyModel[] = [];
+    
+    const collection = MongoHelper.getCollection('surveys');
+    const surveysList= await collection.find().toArray();
+
+    for (const survey of surveysList) res.push(MongoHelper.map(survey)); 
+    return res.length > 0 ? res : null; 
   }
 }
