649472830b32719aea2baedc15de691675c846ac
Jean Fraga <jeandevjs@gmail.com>
2023-03-17 21:38:59 -0300

    refactor: surveys routes file

--- src/main/routes/load-surveys-route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { makeRouteAdapter } from "../apapters/express-adapter";
-import { makeLoadSurveysController } from "../factories/survey/load-surveys/load-surveys-controller-factory";
-
-export default (router: Router): void => {
-  router.get('/load-surveys', makeRouteAdapter(makeLoadSurveysController())); 
-}; 
