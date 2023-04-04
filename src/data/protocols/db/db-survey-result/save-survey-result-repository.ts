b01b7f531f34dec4a67375350ccbc54641a30e37
Jean Fraga <jeandevjs@gmail.com>
2023-04-03 22:12:35 -0300

    refactor: change SaveSurveyResultRepository to returns void

--- src/data/protocols/db/db-survey-result/save-survey-result-repository.ts
+++ src/data/protocols/db/db-survey-result/save-survey-result-repository.ts
@@ -1,6 +1,5 @@
 import { AddSurveyResultModel } from '../../../../domain/usecases/save-survey-result';
-import { SurveyResultModel } from '../../../../domain/models/survey-result-model';
 
 export interface SaveSurveyResultRepository {
-  saveResult (data: AddSurveyResultModel): Promise<SurveyResultModel>
+  saveResult (data: AddSurveyResultModel): Promise<void>
 }
