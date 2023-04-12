b860885642e121c9a910c80fc460d83073918d37
Jean Fraga <jeandevjs@gmail.com>
2023-04-12 20:59:04 -0300

    feat: ensure LoadSurveyResultController calls load LoadSurveyResult with correct values

--- src/presentation/controller/survey/load-survey-result-controller/load-survey-result-controller-protocols.ts
+++ src/presentation/controller/survey/load-survey-result-controller/load-survey-result-controller-protocols.ts
@@ -1,5 +1,7 @@
 export * from '../../../../domain/models/survey-model';
 export * from '../../../../domain/usecases/load-survey-by-id';
+export * from '../../../../domain/models/survey-result-model';
+export * from '../../../../domain/usecases/load-survey-result'; 
 export * from '../../../protocols';
 export * from '../../../http-helpers/http-helpers';
 export * from '../../../errors';
--- src/presentation/controller/survey/load-survey-result-controller/load-survey-result-controller.ts
+++ src/presentation/controller/survey/load-survey-result-controller/load-survey-result-controller.ts
@@ -1,15 +1,18 @@
-import { Controller, forbidden, HttpRequest, HttpResponse, InvalidParamError, LoadSurveyById, serverError } from "./load-survey-result-controller-protocols";
+import { Controller, forbidden, HttpRequest, HttpResponse, InvalidParamError, LoadSurveyById, LoadSurveyResult, serverError } from "./load-survey-result-controller-protocols";
 
 export class LoadSurveyResultController implements Controller {
   constructor (
-    private readonly loadSurveyById: LoadSurveyById
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try { 
       const { surveyId } = httpRequest.params;
+      const { accountId } = httpRequest;
       const survey = await this.loadSurveyById.load(surveyId);
-      if (!survey) return forbidden(new InvalidParamError('surveyId'))
+      if (!survey) return forbidden(new InvalidParamError('surveyId'));
+      this.loadSurveyResult.load(survey.id, accountId); 
       return null
 
     } catch ( error ) {
