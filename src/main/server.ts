270696af060b4ccbeb1777fcceb102fb3361ffda
Jean Fraga <jeanfragoli395@gmail.com>
2023-12-01 13:57:30 -0300

    chores: add rimraf build and pm2

--- src/main/server.ts
+++ src/main/server.ts
@@ -5,7 +5,7 @@ import env from './config/env';
    MongoHelper.connect(env.dbUri)
     .then(async ()=> {
       const app = (await import("./config/app")).default
-      app.listen(env.port, () => console.log(`App run on http://localhost:${env.port}`));
+      app.listen(env.port, () => console.log(`App run on ${env.port} 🦄`));
   }).catch(err => console.log(err));
   
  
