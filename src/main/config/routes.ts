fa9c0f414f8c812704d66b38b2732c0d8aa0004d
Jean Fraga <jeanfragoli395@gmail.com>
2023-12-01 15:04:10 -0300

    feat: add welcome api

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,12 @@
-import { Express, Router } from 'express';
+import { Express, Response, Router, Request } from 'express';
 import { readdirSync } from 'fs';
 
 export default (app:Express) : void => {
   const router = Router();
-  app.use('/clean-api', router);
+  app.get('/', (req: Request, res: Response) => {
+    return res.json({message: "Welcome to Survey API 🥳"})
+  })
+  app.use('/survey-api', router);
 
   readdirSync(`${__dirname}/../routes`).map(async file => {
     const isFileTestOrBuild = file.includes('.test') || file.includes('.map');
