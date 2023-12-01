8ecc390b20f3cb5e54570d1a0bad7fc364ba8b04
Jean Fraga <72657296+devjeanfraga@users.noreply.github.com>
2023-12-01 08:47:56 -0300

    Update env.ts

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   port: process.env.PORT || 5050,
-  dbUri: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-survey-api',
+  dbUri: process.env.MONGO_URL,
   salt: parseInt(process.env.SALT) || 12 ,
   secretKey: process.env.SECRET_KEY || 'secret-key-test'
 };
