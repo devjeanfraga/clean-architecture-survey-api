133bb2258b7d338a19592d8e12379c0318fc2a9f
Jean Fraga <72657296+devjeanfraga@users.noreply.github.com>
2023-12-01 08:52:14 -0300

    Update env.ts

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   port: process.env.PORT || 5050,
-  dbUri: process.env.MONGO_URL || "mongodb+srv://jeanfraga:admin@cluster0.tivsig0.mongodb.net/?retryWrites=true&w=majority",
+  dbUri: process.env.MONGO_URL,
   salt: parseInt(process.env.SALT) || 12 ,
   secretKey: process.env.SECRET_KEY || 'secret-key-test'
 };
