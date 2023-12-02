d35cfb94a5f96c031f4f51379907d835f147a10d
Jean Fraga <jeanfragoli395@gmail.com>
2023-12-02 09:25:08 -0300

    chores: update eslint and eslint config, change project name clean-api to survey-api

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,10 @@ module.exports = {
   root: true,
   rules: {
     "no-empty-function": "off",
+    "no-var": "off",
+    "no-undef": "off",
     "@typescript-eslint/no-empty-function": "off",
-    "no-var": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off"
   }
 };
